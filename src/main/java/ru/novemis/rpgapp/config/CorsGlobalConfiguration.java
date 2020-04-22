8b07dac9c21a7d8860207082260df5d6db985466
n.kombarov <n.kombarov@pflb.ru>
2020-04-22 21:32:41 +0000

    сраный корс отключен, слава Аду!

--- src/main/java/ru/novemis/rpgapp/config/CorsGlobalConfiguration.java
+++ src/main/java/ru/novemis/rpgapp/config/CorsGlobalConfiguration.java
@@ -2,28 +2,25 @@ package ru.novemis.rpgapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
-import java.util.Arrays;
 
 @Configuration
 @EnableWebFlux
 public class CorsGlobalConfiguration {
 
     @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://novemis.ru:3000"));
-        corsConfiguration.setMaxAge(8000L);
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "OPTIONS"));
+    public WebFluxConfigurer corsConfigurer() {
+        return new WebFluxConfigurerComposite() {
 
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/** ** ", corsConfiguration);
-
-        return new CorsWebFilter(source);
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
     }
 }
