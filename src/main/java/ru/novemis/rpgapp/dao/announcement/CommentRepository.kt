1d2e08ac5ec4a61a49bdc2b6a2e57cac9cd6ea47
n.kombarov <n.kombarov@pflb.ru>
2020-04-29 22:31:22 +0000

    реализовал удаление комента по базе

--- src/main/java/ru/novemis/rpgapp/dao/announcement/CommentRepository.kt
+++ src/main/java/ru/novemis/rpgapp/dao/announcement/CommentRepository.kt
@@ -1,13 +1,16 @@
 package ru.novemis.rpgapp.dao.announcement
 
 import org.springframework.data.domain.Sort
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import ru.novemis.rpgapp.domain.announcement.comment.Comment
 
 interface CommentRepository : CrudRepository<Comment, String> {
 
+    @Query("select c from Comment c where c.deleted = false")
     fun findByAnnouncementId(announcementId: String, sort: Sort): List<Comment>
 
+    @Query("select count(c) from Comment c where c.deleted = false")
     fun countByAnnouncementId(announcementId: String): Long
 
 }
