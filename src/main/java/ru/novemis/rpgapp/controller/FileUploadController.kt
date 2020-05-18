d51ec89a734393eabb1db0354af727a78a5f2a6d
nikolay <kombarov55@gmail.com>
2020-05-18 03:47:18 +0300

    неудачная попытка сделать мультипарт

--- src/main/java/ru/novemis/rpgapp/controller/FileUploadController.kt
+++ src/main/java/ru/novemis/rpgapp/controller/FileUploadController.kt
@@ -1,11 +1,14 @@
 package ru.novemis.rpgapp.controller
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import ru.novemis.rpgapp.dto.common.UploadRsDto
 import ru.novemis.rpgapp.repository.announcement.UploadCacheRepository
-import java.io.File
+
 
 @RestController
 @RequestMapping("/upload")
@@ -16,8 +19,8 @@ class FileUploadController(
         private val uploadCacheRepository: UploadCacheRepository
 ) {
 
-    @PostMapping
-    fun upload(@RequestParam("file") file: MultipartFile): UploadRsDto {
+    @PostMapping(consumes = ["multipart/form-data"])
+    fun upload(@RequestPart(value = "file") file: Array<MultipartFile>): UploadRsDto {
         return UploadRsDto("hui-na-blude.jpg")
     }
 
