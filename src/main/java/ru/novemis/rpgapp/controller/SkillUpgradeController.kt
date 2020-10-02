761f61194389ee70cb605846117e63205441c3af
18456857 <nakombarov@sberbank.ru>
2020-10-02 16:29:02 +0300

    crud повышения навыка

--- src/main/java/ru/novemis/rpgapp/controller/SkillUpgradeController.kt
+++ src/main/java/ru/novemis/rpgapp/controller/SkillUpgradeController.kt
@@ -1,10 +1,9 @@
 package ru.novemis.rpgapp.controller
 
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import ru.novemis.rpgapp.converter.SkillUpgradeConverter
 import ru.novemis.rpgapp.dto.game.skill.dto.SkillUpgradeDto
+import ru.novemis.rpgapp.dto.game.skill.form.SkillUpgradeForm
 import ru.novemis.rpgapp.repository.game.skillcategory.SkillRepository
 import ru.novemis.rpgapp.repository.game.skillcategory.SkillUpgradeRepository
 import javax.transaction.Transactional
@@ -16,6 +15,38 @@ open class SkillUpgradeController(
 
         private val skillRepository: SkillRepository
 ) {
+
+    @PostMapping("/skill/{skill-id}/skillUpgrade")
+    @Transactional
+    open fun addSkillUpgradeToSkill(
+            @PathVariable("skill-id") skillId: String,
+            @RequestBody form: SkillUpgradeForm
+    ): SkillUpgradeDto {
+        val skill = skillRepository.findById(skillId).get()
+
+        return converter.toDomain(form, skill.skillCategory!!.game!!.id).apply {
+            this.skill = skill
+        }.let { repository.save(it) }.let { converter.toDto(it) }
+    }
+
+    @PutMapping("/skillUpgrade/{id}")
+    @Transactional
+    open fun upgradeSkillUpgrade(
+            @PathVariable("id") id: String,
+            @RequestBody form: SkillUpgradeForm
+    ): SkillUpgradeDto {
+        val savedEntity = repository.findById(id).get()
+        val skill = savedEntity.skill!!
+        val convertedSkillUpgrade = converter.toDomain(form, skill.skillCategory!!.game!!.id)
+
+        savedEntity.apply {
+            description = convertedSkillUpgrade.description
+            prices = convertedSkillUpgrade.prices
+        }
+
+        return repository.save(savedEntity)
+                .let { converter.toDto(it) }
+    }
 
     @DeleteMapping("/skillUpgrade/{id}")
     @Transactional
@@ -32,5 +63,4 @@ open class SkillUpgradeController(
 
         return converter.toDto(skillUpgrade)
     }
-
 }
