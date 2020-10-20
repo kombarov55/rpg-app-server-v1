f001ab6f10b025e38c72f4c47834dcafd4df466e
nikolay <kombarov55gmail.com>
2020-10-20 23:52:35 +0300

    синхронизирующися автокомплит

--- src/main/java/ru/novemis/rpgapp/controller/GameCharacterController.kt
+++ src/main/java/ru/novemis/rpgapp/controller/GameCharacterController.kt
@@ -1,7 +1,9 @@
 package ru.novemis.rpgapp.controller
 
+import org.springframework.data.domain.PageRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import ru.novemis.rpgapp.converter.GameCharacterConverter
 import ru.novemis.rpgapp.dto.game.character.dto.GameCharacterDto
@@ -30,6 +32,20 @@ open class GameCharacterController(
             @PathVariable("user-id") userId: Long
     ): List<GameCharacterShortDto> {
         return repository.findByUserId(userId).map { converter.toShortDto(it) }
+    }
+
+    @GetMapping("/game/{id}/character/filter")
+    @Transactional
+    open fun findCharacterByName(
+            @PathVariable("id") gameId: String,
+            @RequestParam("name") name: String
+    ): List<GameCharacterShortDto> {
+        return repository.findByGameIdAndNameStartsWith(gameId, name, PageRequest.of(0, 5)).map { character ->
+            GameCharacterShortDto(
+                    id = character.id,
+                    name = character.name
+            )
+        }
     }
 
 }
--- src/main/java/ru/novemis/rpgapp/repository/game/character/GameCharacterRepository.kt
+++ src/main/java/ru/novemis/rpgapp/repository/game/character/GameCharacterRepository.kt
@@ -1,12 +1,15 @@
 package ru.novemis.rpgapp.repository.game.character
 
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import ru.novemis.rpgapp.domain.game.character.GameCharacter
 
-interface GameCharacterRepository : CrudRepository<GameCharacter, String> {
+interface GameCharacterRepository : PagingAndSortingRepository<GameCharacter, String> {
 
     @Query("select gc from GameCharacter gc where gc.owner.userId = :userId")
     fun findByUserId(userId: Long): List<GameCharacter>
+
+    fun findByGameIdAndNameStartsWith(gameId: String, name: String, pageable: Pageable): List<GameCharacter>
 
 }
