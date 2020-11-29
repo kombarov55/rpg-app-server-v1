c08e13f41fd3ed393cc528decfda9194321aa7d0
nikolay <kombarov55@gmail.com>
2020-11-29 04:32:22 +0300

    необязательность описания повышения скила

--- src/main/java/ru/novemis/rpgapp/dto/game/skill/form/SkillUpgradeForm.kt
+++ src/main/java/ru/novemis/rpgapp/dto/game/skill/form/SkillUpgradeForm.kt
@@ -4,6 +4,6 @@ import ru.novemis.rpgapp.dto.game.common.form.PriceForm
 
 data class SkillUpgradeForm(
         var lvlNum: Int = 0,
-        var description: String = "",
+        var description: String? = "",
         var prices: List<List<PriceForm>> = mutableListOf()
 )
